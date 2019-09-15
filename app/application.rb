2f9e550798e314230dca832f1e16dbe759be7ab4
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-09-15 19:21:15 -0400

    finished dynamic

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,24 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    
+    num1 = Kernel.rand(1..2)
+    num2 = Kernel.rand(1..2)
+    num3 = Kernel.rand(1..2)
+
+    resp.write "#{num1}\n"
+
+    resp.write "#{num2}\n"
+    resp.write "#{num3}\n"
+
+    if num1==num2 && num2==num3
+      resp.write "you win, your prize is nothing"
+    else
+      resp.write "your a loser, baby, so why you kill me"
+    end
+
     resp.finish
+
   end
 
 end
